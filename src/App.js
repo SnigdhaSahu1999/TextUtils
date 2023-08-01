86eed666b4c6070d53a4341be9d9036d81aaa6a0
Snigdha <snigdha.sahu1999@gmail.com>
2023-08-01 11:39:58 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+// the above import is for css file
+import Navbar from './components/Navbar';
+
+import TextForm from './components/TextForm';
+// we have created a Navbar file inside components folder
+
+//import About from './components/About';
+import React, {useState} from 'react';
+//import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 
 function App() {
+
+  const [mode, setMode]  =  useState('light'); 
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    }
+    else{
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        {/* We send the values to the Navbar() of Navbar.js by "props" parameter*/}
+        {/* <Navbar></Navbar> */}
+      {/*<Navbar title="TextUtils" aboutText="About TextUtils"></Navbar>*/}
+
+      
+      {/* <BrowserRouter> */}
+      <Navbar title="TextUtils"  mode={mode} toggleMode={toggleMode}></Navbar>
+      <div className="container my-3" >
+          {/* <Routes>
+            <Route exact path="/about" element={<About/>} />
+          </Routes>
+          <Routes>
+            <Route path="/" element={ <TextForm heading="Enter the text to analyze below" mode={mode}/> }/>
+          </Routes> */}
+          <TextForm heading="Enter the text to analyze below" mode={mode}/>
+      </div>
+      {/* </BrowserRouter> */}
+
+      {/*<div className="container my-3">
+        <About/>
+      </div>*/}
+    </>
+
   );
 }
 
